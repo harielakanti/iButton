30edceda1826964296fb1395ad8c131788ce611c
harielakanti <harielakanti@gmail.com>
2019-12-19 14:31:19 +0530

    Framework Added

---
+++ iButton/iButton/iButton.h
@@ -0,0 +1,19 @@
+//
+//  iButton.h
+//  iButton
+//
+//  Created by Hari_Vrinda on 12/19/19.
+//  Copyright © 2019 Hari_Vrinda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iButton.
+FOUNDATION_EXPORT double iButtonVersionNumber;
+
+//! Project version string for iButton.
+FOUNDATION_EXPORT const unsigned char iButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iButton/PublicHeader.h>
+
+
