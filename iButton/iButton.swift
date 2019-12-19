a6588af998c6f11ab16ac0c86fa5ea563e684526
harielakanti <harielakanti@gmail.com>
2019-12-19 19:33:08 +0530

    added runtime attributes

--- iButton/iButton.swift
+++ iButton/iButton.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-public class iButton: UIButton {
+@IBDesignable public class iButton: UIButton {
     
     public typealias DidTapButton = (iButton) -> ()
     
@@ -29,4 +29,52 @@ public class iButton: UIButton {
         }
     }
     
+    @IBInspectable var cornerRadius: Double {
+        get {
+            return Double(self.layer.cornerRadius)
+        }set {
+            self.layer.cornerRadius = CGFloat(newValue)
+        }
+    }
+    @IBInspectable var borderWidth: Double {
+        get {
+            return Double(self.layer.borderWidth)
+        }
+        set {
+            self.layer.borderWidth = CGFloat(newValue)
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue?.cgColor
+        }
+    }
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.shadowColor!)
+        }
+        set {
+            self.layer.shadowColor = newValue?.cgColor
+        }
+    }
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return self.layer.shadowOpacity
+        }
+        set {
+            self.layer.shadowOpacity = newValue
+        }
+    }
+    @IBInspectable var shadowOffset: CGPoint {
+        get {
+            return CGPoint(x: layer.shadowOffset.width, y:layer.shadowOffset.height)
+        }
+        set {
+            self.layer.shadowOffset = CGSize(width: newValue.x, height: newValue.y)
+        }
+        
+    }
 }
--- iButtonExample/ViewController.swift
+++ iButtonExample/ViewController.swift
@@ -27,6 +27,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
